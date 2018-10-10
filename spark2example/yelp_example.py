235a43d7bab539ed327be882bf59e24c3f8a5b5f
Mikel Emaldi <m.emaldi@gmail.com>
2018-10-10 11:24:35 +0200

    Enabling argument parsing for Spark2 example

--- spark2example/yelp_example.py
+++ spark2example/yelp_example.py
@@ -1,9 +1,18 @@
+import argparse
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("YelpExample").getOrCreate()
-business_df = spark.read.csv('/user/mikel/samples/yelp_business.csv',
+parser = argparse.ArgumentParser(description='Execute Spark2 Yelp example.')
+parser.add_argument(
+    '--app-name', type=str, help="Application name", default='YelpExample')
+parser.add_argument('input-file', type=str, help="Input CSV file")
+parser.add_argument('output-dir', type=str, help="Output directory")
+
+args = parser.parse_args()
+
+spark = SparkSession.builder.appName(args['app-name']).getOrCreate()
+business_df = spark.read.csv(args['input-file'],
                              header=True, quote='"', escape='"')
 
 state_count = business_df.groupBy(business_df.state).count()
 sorted_state_count = state_count.sort("count", ascending=False)
-sorted_state_count.write.csv('/user/mikel/spark-csv-output')
+sorted_state_count.write.csv(args['output-dir'])

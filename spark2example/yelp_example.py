095cd01f892c60aa20d7b6b88fe1f90b97fe523d
Mikel Emaldi <m.emaldi@gmail.com>
2018-10-10 11:33:08 +0200

    Fixing bugs

--- spark2example/yelp_example.py
+++ spark2example/yelp_example.py
@@ -3,16 +3,16 @@ from pyspark.sql import SparkSession
 
 parser = argparse.ArgumentParser(description='Execute Spark2 Yelp example.')
 parser.add_argument(
-    '--app-name', type=str, help="Application name", default='YelpExample')
-parser.add_argument('input-file', type=str, help="Input CSV file")
-parser.add_argument('output-dir', type=str, help="Output directory")
+    '--app_name', type=str, help="Application name", default='YelpExample')
+parser.add_argument('input_file', type=str, help="Input CSV file")
+parser.add_argument('output_dir', type=str, help="Output directory")
 
 args = parser.parse_args()
 
-spark = SparkSession.builder.appName(args['app-name']).getOrCreate()
-business_df = spark.read.csv(args['input-file'],
+spark = SparkSession.builder.appName(args.app_name).getOrCreate()
+business_df = spark.read.csv(args.input_file,
                              header=True, quote='"', escape='"')
 
 state_count = business_df.groupBy(business_df.state).count()
 sorted_state_count = state_count.sort("count", ascending=False)
-sorted_state_count.write.csv(args['output-dir'])
+sorted_state_count.write.csv(args.output_dir)

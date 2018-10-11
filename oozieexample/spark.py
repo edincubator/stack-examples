71a3ad8672e47ed4edf2b63ba2f50e89ee818fb3
Mikel Emaldi <m.emaldi@gmail.com>
2018-10-11 13:33:18 +0200

    Fixing oozie issues

--- oozieexample/spark.py
+++ oozieexample/spark.py
@@ -1,7 +1,25 @@
+import argparse
+
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("SparkOozieExample").getOrCreate()
+parser = argparse.ArgumentParser(description='Execute Spark2 Oozie example.')
+parser.add_argument(
+    '--app_name', type=str, help="Application name",
+    default='SparkOozieExample')
+parser.add_argument('username', type=str, help="User launching the job")
+parser.add_argument('example_dir', type=str, help="Oozie example dir")
 
-business_count = spark.read.csv('/user/docuser/pig-output/part-r-00000')
+
+args = parser.parse_args()
+
+spark = SparkSession.builder.appName(args.app_name).getOrCreate()
+
+business_count = spark.read.csv(
+    '/user/{username}/{example_dir}/pig-output/part-r-00000'.format(
+        username=args.username, example_dir=args.example_dir
+    ))
 business_filtered = business_count.filter(business_count._c1 > 1000)
-business_filtered.write.csv('/user/docuser/spark-oozie-output')
+business_filtered.write.csv(
+    '/user/{username}/{example_dir}/spark-oozie-output'.format(
+        username=args.username, example_dir=args.example_dir
+    ))
